2ca321a2df12b738dbdf2c55c144ae0c4967fc92
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2024-03-20 18:54:07 -0300

    feat/implement new routes and implement prefix to api routes and websocket endpoint

--- routes/api.go
+++ routes/api.go
@@ -2,20 +2,57 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 	"net/http"
 	"time"
+)
+
+var (
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
 )
 
 func Api() {
 	route := gin.Default()
 
-	route.GET("/ping", func(c *gin.Context) {
+	prefix := route.Group("/api/")
+
+	prefix.GET("ping", handlePing)
+	prefix.GET("websocket", handleWebsocket)
+	route.Run()
+}
+
+func handlePing(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message":   "pong",
+		"status":    http.StatusOK,
+		"timestamp": time.Now().Format("2006-01-02 15:04:05"),
+	})
+}
+
+func handleWebsocket(c *gin.Context) {
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	defer conn.Close()
+
+	for {
+		_, message, err := conn.ReadMessage()
+
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+
 		c.JSON(http.StatusOK, gin.H{
-			"message":   "pong",
-			"status":    http.StatusOK,
+			"message":   string(message),
 			"timestamp": time.Now().Format("2006-01-02 15:04:05"),
 		})
-	})
-
-	route.Run()
+	}
 }
